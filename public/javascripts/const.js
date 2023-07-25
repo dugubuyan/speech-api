faa3a57c425f595c039299624ca9da5f8614f68e
Alex <alex@alexdeair.chainfeeds.org.github.beta.tailscale.net>
2023-07-25 11:56:40 +0800

    rm logs

--- public/javascripts/const.js
+++ public/javascripts/const.js
@@ -1,10 +1,9 @@
 const os = require('os');
 function getIpAddress() {
     let ifaces = os.networkInterfaces()
-    console.info("****************",ifaces);
     for (let dev in ifaces) {
         let iface = ifaces[dev]
-        console.info(dev, "====>",iface)
+        // console.info(dev, "====>",iface)
         for (let i = 0; i < iface.length; i++) {
             let { family, address, internal,netmask } = iface[i]
             // console.log(address)
@@ -18,7 +17,7 @@ const baseurl = 'http://' + getIpAddress() + ':3000'
 function getStaticUrl(){
     return baseurl + "/uploads/";
 }
-console.log(getStaticUrl())
+// console.log(getStaticUrl())
 function test(){
     const str = "12434.xyz-789"
     console.log(str.replace(".xyz",""))
